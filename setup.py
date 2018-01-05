33ef9f1c545e34e3106b571d963d9828361d7a53
Mike Gilbert <floppym@gentoo.org>
2018-01-05 13:32:45 -0500

    setup.py: exit with non-zero status on failure
    https://bugs.gentoo.org/show_bug.cgi?id=281968
    https://bugs.python.org/issue6731

--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ Programming Language :: Python
 Topic :: Software Development
 """
 
+exit_status = 0
 
 def run_command(cmd):
     status = os.system(cmd)
@@ -539,8 +540,10 @@ class PyBuildExt(build_ext):
             print("been built, they are *disabled* by configure:")
             print_three_column(self.disabled_configure)
             print()
+        global exit_status
 
         if self.failed:
+            exit_status = 1
             failed = self.failed[:]
             print()
             print("Failed to build these modules:")
@@ -548,6 +551,7 @@ class PyBuildExt(build_ext):
             print()
 
         if self.failed_on_import:
+            exit_status = 1
             failed = self.failed_on_import[:]
             print()
             print("Following modules built successfully"
@@ -2618,6 +2622,7 @@ def main():
           scripts = ["Tools/scripts/pydoc3", "Tools/scripts/idle3",
                      "Tools/scripts/2to3"]
         )
+    sys.exit(exit_status)
 
 # --install-platlib
 if __name__ == '__main__':
